27e1b580f27c1925563b6c9ba28c8aa04b3b33a9
JUCA-CG <90279489+JUCA-CG@users.noreply.github.com>
2021-10-21 00:16:19 -0500

    Add files via upload

--- Actividad_8F/src/actividad_8f/Actividad_8F.java
+++ Actividad_8F/src/actividad_8f/Actividad_8F.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package actividad_8f;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -12,7 +15,7 @@ import java.util.Iterator;
  * @author jc_ca
  */
 public class Actividad_8F {
-
+public static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));  
     /**
      * @param args the command line arguments
      */
@@ -36,7 +39,8 @@ public class Actividad_8F {
         }
             deck.add("Palo: "+card.palo[j]+ ", Color: "+color + ", Valor: "+ card.valor[i]); }        }
 
-       
+             
+        int deck1 = showMenu(deck);
        /* Iterator<String> nombreIterator = deck.iterator();
         int cont =1;
        while(nombreIterator.hasNext()){
@@ -44,14 +48,67 @@ public class Actividad_8F {
             System.out.println(cont+".-"+elemento);
            cont++;
         }*/
-        Deck llamada = new Deck();
-        llamada.shuffle(deck);
-        llamada.head(deck);
-        llamada.pick(deck);
-        llamada.hand(deck);
+     
         }catch (Exception e){
             System.out.println("Ha sucedido un error, vuelva intentar");   
         }
    } 
-}
+    private static int showMenu(ArrayList deck) throws IOException {
+        int m;
+    System.out.println("Bienvenido a Poker!" );
+    do{
+        m = 0;
+        boolean e = true;
+System.out.println("Selecciona una opción:\n" +
+"1-Mezclar deck\n" +
+"2-Sacar una carta\n" +
+"3-Carta al azar\n" +
+"4-Generar una mano de 5 cartas\n" +
+"0-Salir");
+      Deck llamada = new Deck();
+  int opcion = Integer.parseInt(entrada.readLine());
+   switch(opcion){
+       case 0:
+           System.out.println("Haz elegido salir\n"+"Gracias por utilizar el programa");
+           
+           return m;
+          
+        case 1:
+            System.out.println("Haz elegido mezclar deck");
+            llamada.shuffle(deck);
+           break;
+        case 2:
+            System.out.println("Haz elegido sacar una carta");
+            llamada.head(deck);
+           break;
+        case 3:
+            System.out.println("Haz elegido una carta al azar");
+            llamada.pick(deck);
+           break;
+           
+        case 4:
+            System.out.println("Haz elegido generar una mano de 5 cartas");
+            llamada.hand(deck);
+           break;
+           
+         default: System.out.println("opción no válida, favor de elegir otra opción");
+         m = 1;
+         e = false;
+             break;
    
+           
+   }
+   if(e ==true){
+   System.out.println("Presione (1) para volver a elegir una opción del menú o cualquier otro número para salir");
+   int o =Integer.parseInt(entrada.readLine());
+   if (o==1){
+       m=1;
+   }}
+    }while(m==1);
+    System.out.println("Gracias por utilizar el programa");
+    return m;
+       
+        
+    }
+}
+    
